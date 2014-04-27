f6151c65b734302086548ae2d9bb9f9f31e6f267
Eric Tseng <ericdasyo@gmail.com>
2014-04-27 11:46:32 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,28 +3,53 @@
 
 ## Write a short comment describing this function
 
+
+## this function returns a list of function
+
 makeCacheMatrix <- function(x = matrix()) {
+
+## setting value for the matrix, will store different variable including cahched.
+
   p<-Null
   set <- function (y) {
     x<<-y
     p<<-NULL
   }
-  
+
+## getting the value
+
   get<-function() x
+
+## setting for the inversed matrix
+
   setmat <-function(mat) p <<-mat
   getmat <-function ()p
+
+## getting the inversed matrix list
   list (set= set, get = get,
         setmat=setmat, getmat=getmat)
 }
 
+
+## Return inversed matrix with cached value
+
   cacheSolve <- function(x, ...) {
     p<- x$getmat()
-    if(!is.null(p)){
+
+## check if already calculated
+
+if(!is.null(p)){
       message("getting cached data")
       return (p)
     }
+
+## calc it if not yet done
+
     data <-x$get()
     p <- solve(data, ...)
+
+## set the inversed mat
+
     x$setmat(p)
     p
     
